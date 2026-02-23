36c751b12350911284ff49fe3269194f0ea05800
voho <vojtech.hordejcuk@gmail.com>
2026-02-23 15:10:47 +0100

    v1

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Navbar Scrolled State
+    const navbar = document.getElementById('navbar');
+    
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
